91ddb5eba4cd4c8db6e1ef3af1c8be0abbfaf45d
Joy Goswami <joygoswaminiloy2023@gmail.com>
2026-02-23 00:51:48 +0600

    makung a object with suitable data and push into an array by event elegation

--- script.js
+++ script.js
@@ -69,12 +69,136 @@ else if(btn_id=='reject_btn'){
 cardsElement.classList.add('hidden');
 filter_sec.classList.remove('hidden');
 inter_display.innerText = interview_data.length;
-
+fetch_data_reject();  
+ blank_sec();    
 
    }
   
 
 
   }
+  
+
+  maincontainer.addEventListener('click',function(event){
+
+  // interview_btn
+  if(event.target.classList.contains('interview_btn')){
+
+
+    const jobdata=event.target.parentNode.parentNode;
+
+const jobname=jobdata.querySelector('.jobName').innerText;
+const typeofJob=jobdata.querySelector('.typeOfJob').innerText;
+const timming=jobdata.querySelector('.timming').innerText;
+const timming_1=jobdata.querySelector('.timming_1').innerText;
+const timming_2=jobdata.querySelector('.timming_2').innerText;
+const salary=jobdata.querySelector('.salary').innerText;
+const status=jobdata.querySelector('.status').innerText;
+const des=jobdata.querySelector('.des').innerText;
+jobdata.querySelector('.status').innerText="Interview"
+
+const cardInfo={
+  jobname,
+  typeofJob,
+  timming,
+  status:'Interview',
+  timming_1,
+  timming_2,
+  salary,
+  des
+
+
+
+}
+
+const is_in=interview_data.find(item=> item.jobname==cardInfo.jobname);
+
+
+
+if(!is_in){
+  interview_data.push(cardInfo);
+}
+
+if(jobdata.parentNode === cardsElement){
+      cardsElement.removeChild(jobdata);
+  }
+
+reject_data=reject_data.filter(item=>item.jobname!=cardInfo.jobname);
+
+if(current_status=='reject_btn'){
+  fetch_data_reject();
+}
+
+
+inter_display.innerText = interview_data.length;
+
+
+
+  }
+
+// reject_btn
+    else if(event.target.classList.contains('reject_btn')){
+
+
+    const jobdata=event.target.parentNode.parentNode;
+
+const jobname=jobdata.querySelector('.jobName').innerText;
+const typeofJob=jobdata.querySelector('.typeOfJob').innerText;
+const timming=jobdata.querySelector('.timming').innerText;
+const timming_1=jobdata.querySelector('.timming_1').innerText;
+const timming_2=jobdata.querySelector('.timming_2').innerText;
+const salary=jobdata.querySelector('.salary').innerText;
+const status=jobdata.querySelector('.status').innerText;
+const des=jobdata.querySelector('.des').innerText;
+jobdata.querySelector('.status').innerText="Rejected"
+
+const cardInfo={
+  jobname,
+  typeofJob,
+  timming,
+  status:'Rejected',
+  timming_1,
+  timming_2,
+  salary,
+  des
+
+
+
+}
+
+const is_in=reject_data.find(item=> item.jobname==cardInfo.jobname);
+
+
+
+if(!is_in){
+  reject_data.push(cardInfo);
+}
+
+if(jobdata.parentNode === cardsElement){
+      cardsElement.removeChild(jobdata);
+  }
+
+interview_data=interview_data.filter(item=>item.jobname!=cardInfo.jobname);
+inter_display.innerText = interview_data.length;
+
+if(current_status=='interview_btn'){
+  fetch_data_interview();
+}
+
+
+
+reject_display.innerText = reject_data.length;
+
+ }
+
+
+
+})
+
+
+
+
+
+
 
 
